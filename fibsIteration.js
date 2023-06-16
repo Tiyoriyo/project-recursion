e4fe49e2f06e5f9000fb8d49184cfc799b65789b
Vadim Kostrov <vmkvo15@outlook.com>
2023-06-16 18:47:17 +0400

    Add iteration fibonacci code to file

--- fibsIteration.js
+++ fibsIteration.js
@@ -1,22 +1,19 @@
-#!/usr/bin/node
+function fibsIteration(n) {
 
-function fibsRec(n) {
-    if (n === 0) return [];
-    if (n === 1) return [0];
-    if (n === 2) return fibsRec(n-1).concat([1]);
-    
+    let array = [];
 
-    let array = fibsRec(n-1);
-    let a = fibsRec(n-1).pop();
-    let b = fibsRec(n-2).pop(); 
-    let c = a + b;
+    for (let i = 0; i < n; i++) {
+        if (i === 0 || i === 1) {
+            array.push(i);
+        } else {
+            array.push(array[i - 1] + array[i - 2]);
+        }
+    }
 
-    array = array.concat(c);
-    
     return array;
 }
 
-console.log(fibsRecurse(0))
-console.log(fibsRecurse(5))
-console.log(fibsRecurse(3))
-console.log(fibsRecurse(10))
+console.log(fibsIteration(0))
+console.log(fibsIteration(5))
+console.log(fibsIteration(3))
+console.log(fibsIteration(10))
